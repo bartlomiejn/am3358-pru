cb8de12d24f57bf6b8ce56d54794200d90ad92df
Bartlomiej Nowak <bartlomiej.nwk@gmail.com>
2019-01-09 16:29:22 +0000

    Fixed set bit function

--- src/binary.h
+++ src/binary.h
@@ -20,5 +20,5 @@ inline bool ui32_get_nth_bit(uint32_t from, int n)
 
 inline void ui32_set_nth_bit(volatile uint32_t *source, int n, int to)
 {
-    *source = (source & (~(1 << n))) | (to << n);
+    *source = (*source & (~(1 << n))) | (to << n);
 }
