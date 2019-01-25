b166c07b8a3ef8a2ffc10618b2b0b583af1c791d
Bartłomiej Nowak <bartlomiej.nwk@gmail.com>
2019-01-25 11:34:00 +0100

    Fixed config_pru.sh

--- setup/config_pru.sh
+++ setup/config_pru.sh
@@ -14,5 +14,5 @@ config-pin P8_11 pruout
 config-pin P8_15 pruin
 config-pin P8_12 pruout
 config-pin P8_16 pruin
-echo am335x-pru0-fw > sys/class/remoteproc/remoteproc1/firmware
-echo start > sys/class/remoteproc/remoteproc1/state
+echo am335x-pru0-fw > /sys/class/remoteproc/remoteproc1/firmware
+echo start > /sys/class/remoteproc/remoteproc1/state
